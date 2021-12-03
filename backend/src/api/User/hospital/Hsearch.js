9aa24c0e85f6c92fe83b1543fdeb64ae9a988068
mosuhwan <jukiio01@daum.net>
2021-12-03 23:02:02 +0900

    x

--- backend/src/api/User/hospital/Hsearch.js
+++ backend/src/api/User/hospital/Hsearch.js
@@ -1,9 +1,9 @@
 export default (app, conn) =>{
     app.get('/Hsearch', (req,res,next)=>{
-        const {Date,adress1,adress2,adress3} = req.query;
-        console.log(Date,adress1,adress2,adress3);
+        const {Date,address1,address2,address3} = req.query;
+        console.log(Date,address1,address2,address3);
         conn.query("SELECT H.name, H.address1, D.Date FROM hospital as H, officeDate as D, vaccineinfo as V WHERE H.name=D.hospitalName and D.DateId=V.DateId and D.Date=? and H.address1=CONCAT(?,' ',?,' ',?);",
-        [Date,adress1,adress2,adress3],
+        [Date,address1,address2,address3],
         (err,result)=>{
             if(err || result.length===0){
                 res.send({result:false});
