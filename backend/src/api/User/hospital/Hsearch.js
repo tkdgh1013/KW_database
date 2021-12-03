2455becca3ab017be331fc1cd640ef720b1313de
mosuhwan <jukiio01@daum.net>
2021-12-03 22:51:01 +0900

    g

--- backend/src/api/User/hospital/Hsearch.js
+++ backend/src/api/User/hospital/Hsearch.js
@@ -2,7 +2,7 @@ export default (app, conn) =>{
     app.get('/Hsearch', (req,res,next)=>{
         const {Date,adress1,adress2,adress3} = req.query;
         console.log(Date,adress1,adress2,adress3);
-        conn.query("SELECT H.name, H.address1, D.Date FROM hospital as H, officeDate as D, vaccineinfo as V WHERE H.name=D.hospitalName and D.DateId=V.DateId and D.Date='2021-11-01' and H.address1=CONCAT('서울시',' ','금천구',' ','독산동');",
+        conn.query("SELECT H.name, H.address1, D.Date FROM hospital as H, officeDate as D, vaccineinfo as V WHERE H.name=D.hospitalName and D.DateId=V.DateId and D.Date=? and H.address1=CONCAT(?,' ',?,' ',?);",
         [Date,adress1,adress2,adress3],
         (err,result)=>{
             if(err || result.length===0){
