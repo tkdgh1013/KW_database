439dbb1070b4e07323d8d67f7a01f08d6bb66680
hdak95 <37134920+hdak95@users.noreply.github.com>
2021-12-05 03:24:34 +0900

    mypage

--- frontend/src/pages/Mypage.js
+++ frontend/src/pages/Mypage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components'
 import { Link } from 'react-router-dom';
@@ -59,40 +59,44 @@ const Btn = styled.button`
     margin-left: 40px;
     cursor: pointer;
 `
-const loginHandler = ({name, RRN, pn}) => {
-    axios.get("http://localhost:4000/login", {params:{userName : name, RRN: RRN, phoneNumber:pn}}).then(({data})=>{
-        if(data.result===true){
-            document.location.href = '/page2'
-        }
-        else{
-                alert("입력하신 이름과 주민번호, 연락처가 일치하지 않습니다.");
-        }
-    });
-    
-}
+
+
 
 const Mypage= ({history})=>{
-    const [name,setName] = useState("");
-    const [RRN,setRRN] = useState("");
-    const [pn,setPn] = useState("");
-    const onChange1 = (event) => {
-        setName(event.target.value);
-    }
-    const onChange2 = (event) => {
-        setRRN(event.target.value);
-    }
-    const onChange3 = (event) => {
-        setPn(event.target.value);
-    }
-    console.log(name,RRN,pn);
+    
+    const RRN = window.sessionStorage.getItem('RRN');
+
+    const [inputData,setInputData]=useState([{
+        uName:'',
+        hName:'',
+        hContact:'',
+        hAddr1:'',
+        hAddr2:'',
+        oHour:'',
+        dDate:'',
+        rVacc:''
+    }])
+    const[lastIdx,setLastIdx]=useState(0)
+
+    
+    useEffect(async()=>{
+        axios.get("http://localhost:4000/vaccine_info", {params:{RRN: RRN}}).then(({data})=>{
+        
+        if(data.result!==false)
+            console.log(data)
+        
+            
+            
+        });
+    },[])
+    
+
      return (
     <Body><Wrap>
-        <TitleWrap><Title>백신 접종 예약 시스템</Title> </TitleWrap>
+        <TitleWrap><Title>백신 예약 조회 / 취소</Title> </TitleWrap>
+        
         <div
             style={{marginBottom:"60px"}}>
-               
-        <Btn onClick = {()=>loginHandler({name,RRN,pn})}>로그인하기</Btn>
-        <Btn onClick = {()=>{document.location.href = '/signup'}}>회원가입하기</Btn>
         </div>
         </Wrap>
     </Body>
