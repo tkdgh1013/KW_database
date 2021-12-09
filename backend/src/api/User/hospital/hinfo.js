710ee6580d940ac31b70a76a034de57c1bdc86cf
mosuhwan <jukiio01@daum.net>
2021-12-10 01:13:41 +0900

    ddd

--- backend/src/api/User/hospital/hinfo.js
+++ backend/src/api/User/hospital/hinfo.js
@@ -3,7 +3,7 @@ export default (app, conn) =>{
         const {name,DateId} = req.query;
         console.log(name,DateId);
         
-        conn.query("SELECT * FROM hospital as h join officedate as d join officehour as t WHERE h.name=d.hospitalname and d.DateId=t.DateId and t.CanBeReserved=1 and h.name=? and t.DateId=? ",
+        conn.query("SELECT * FROM hospital as h join officedate as d join officehour as t WHERE h.name=d.hospitalname and d.DateId=t.DateId and t.CanBeReserved=1 and h.name=? and t.DateId=?  ",
         [name,DateId],
         (err,result)=>{
             if(err || result.length===0){
